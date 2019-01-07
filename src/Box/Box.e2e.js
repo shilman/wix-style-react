e0c33909a9545fa6c4315f38d13013b986bf0afc
nitayneeman <nitayneeman@users.noreply.github.com>
2019-01-07 16:29:45 +0200

    <Box/> - added e2e tests

--- src/Box/Box.e2e.js
+++ src/Box/Box.e2e.js
@@ -1,22 +1,50 @@
+import { $ } from 'protractor';
+import { waitForVisibilityOf } from 'wix-ui-test-utils/protractor';
 import autoExampleDriver from 'wix-storybook-utils/AutoExampleDriver';
 
 import { eyesItInstance } from '../../test/utils/eyes-it';
 import { createStoryUrl } from '../../test/utils/storybook-helpers';
+import { storySettings } from '../../stories/Box/storySettings';
+
+const baseStorySettings = {
+  kind: storySettings.category,
+  story: storySettings.storyName,
+};
 
 const storyUrl = createStoryUrl({
-  kind: 'Components',
-  story: 'Box',
+  ...baseStorySettings,
   withExamples: false,
+});
+const storyUrlWithExamples = createStoryUrl({
+  ...baseStorySettings,
+  withExamples: true,
 });
 
 const eyes = eyesItInstance();
 
+const byDataHook = dataHook => $(`[data-hook="${dataHook}"]`);
+
 describe('Box', () => {
-  beforeAll(() => browser.get(storyUrl));
+  describe('AutoExample', () => {
+    beforeAll(async () => await browser.get(storyUrl));
 
-  afterEach(async () => await autoExampleDriver.remount());
+    afterEach(async () => await autoExampleDriver.remount());
 
-  eyes.it('should be rendered', () => {
-    expect(true).toBeTruthy();
+    eyes.it('should be rendered', () => {
+      expect(true).toBeTruthy();
+    });
+  });
+
+  describe('Examples', () => {
+    beforeAll(async () => await browser.get(storyUrlWithExamples));
+
+    eyes.it('should render a grid with two aligned inputs (horizontally and vertically)', async () => {
+      const dataHook = 'storybook-box-within-grid';
+      const element = byDataHook(dataHook);
+
+      await waitForVisibilityOf(element, `Cannot find ${dataHook}`);
+
+      expect(true).toBeTruthy();
+    });
   });
 });
