8f59c5ad31ece5199c9054fea246ffc55be6121b
Erez Makavy <erezm@wix.com>
2019-01-07 05:00:46 +0200

    Fix ExamplePresentation

--- stories/MultiSelect/ExamplePresentation.js
+++ stories/MultiSelect/ExamplePresentation.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import MultiSelect from 'wix-style-react/MultiSelect';
 import Heading from 'wix-style-react/Heading';
-import Text from 'wix-style-react/Text';
 import { Layout, Cell } from 'wix-style-react/Layout';
 
 export default () => (
   <Layout>
-    <Cell span="4">
+    <Cell span={4}>
       <Heading appearance="H3">With error</Heading>
     </Cell>
-    <Cell span="8">
+    <Cell span={8}>
       <MultiSelect error errorMessage="This is an error message" />
     </Cell>
-    <Cell span="4">
+    <Cell span={4}>
       <Heading appearance="H3">With various tags</Heading>
     </Cell>
-    <Cell span="8">
+    <Cell span={8}>
       <MultiSelect
         tags={[
-          { id: 1, label: 'Simple Text' },
+          { id: '1', label: 'Simple Text' },
           {
-            id: 2,
+            id: '2',
             label: 'With thumb',
             thumb: (
               <div
@@ -32,22 +31,11 @@ export default () => (
               />
             ),
           },
-          { id: 3, label: 'Error theme', theme: 'error' },
-          { id: 4, label: 'Warning theme', theme: 'warning' },
-          { id: 5, label: 'Non-Removable', removable: false },
-          { id: 6, label: 'Disabled', disabled: true },
-          { id: 7, label: 'Size Large', size: 'large' },
-          {
-            id: 8,
-            label: (
-              <div style={{ width: '200px' }}>
-                <Text ellipsis>
-                  Long Label With Ellipsis To show The Full Label
-                </Text>
-              </div>
-            ),
-            // maxWidth: '200',
-          },
+          { id: '3', label: 'Error theme', theme: 'error' },
+          { id: '4', label: 'Warning theme', theme: 'warning' },
+          { id: '5', label: 'Non-Removable', removable: false },
+          { id: '6', label: 'Disabled', disabled: true },
+          { id: '7', label: 'Size Large', size: 'large' },
         ]}
       />
     </Cell>
