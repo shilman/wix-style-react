74d0502e4aa3056c9562aa453eb1614ab89a0a77
nitayneeman <nitayneeman@users.noreply.github.com>
2019-01-08 14:38:05 +0200

    <Box/> - added additional props

--- src/Box/Box.js
+++ src/Box/Box.js
@@ -4,6 +4,9 @@ import classNames from 'classnames';
 
 import styles from './Box.scss';
 
+const spacingUnit = 6;
+const formatValue = value => isFinite(value) ? value * spacingUnit : `${value}`;
+
 const Box = props => {
   const {
     children,
@@ -11,7 +14,15 @@ const Box = props => {
     align,
     verticalAlign,
     padding,
+    paddingTop,
+    paddingRight,
+    paddingBottom,
+    paddingLeft,
     margin,
+    marginTop,
+    marginRight,
+    marginBottom,
+    marginLeft,
     minWidth,
     maxWidth,
     width,
@@ -23,10 +34,23 @@ const Box = props => {
     [styles.inline]: inline,
   });
   const rootStyles = {
-    alignItems: align,
-    justifyContent: verticalAlign,
-    padding,
-    margin,
+    // Alignment
+    justifyContent: align,
+    alignItems: verticalAlign,
+
+    // Spacing
+    padding: formatValue(padding),
+    paddingTop: formatValue(paddingTop),
+    paddingRight: formatValue(paddingRight),
+    paddingBottom: formatValue(paddingBottom),
+    paddingLeft: formatValue(paddingLeft),
+    margin: formatValue(margin),
+    marginTop: formatValue(marginTop),
+    marginRight: formatValue(marginRight),
+    marginBottom: formatValue(marginBottom),
+    marginLeft: formatValue(marginLeft),
+
+    // Sizing
     minWidth,
     maxWidth,
     width,
@@ -46,14 +70,29 @@ Box.displayName = 'Box';
 
 Box.propTypes = {
   children: PropTypes.node.isRequired,
+  inline: PropTypes,
   align: PropTypes.string,
   verticalAlign: PropTypes.string,
   padding: PropTypes.oneOfType[PropTypes.string, PropTypes.number],
+  paddingTop: PropTypes.oneOfType[PropTypes.string, PropTypes.number],
+  paddingRight: PropTypes.oneOfType[PropTypes.string, PropTypes.number],
+  paddingBottom: PropTypes.oneOfType[PropTypes.string, PropTypes.number],
+  paddingLeft: PropTypes.oneOfType[PropTypes.string, PropTypes.number],
   margin: PropTypes.oneOfType[PropTypes.string, PropTypes.number],
-  width: PropTypes.string,
+  marginTop: PropTypes.oneOfType[PropTypes.string, PropTypes.number],
+  marginRight: PropTypes.oneOfType[PropTypes.string, PropTypes.number],
+  marginBottom: PropTypes.oneOfType[PropTypes.string, PropTypes.number],
+  marginLeft: PropTypes.oneOfType[PropTypes.string, PropTypes.number],
   minWidth: PropTypes.string,
   maxWidth: PropTypes.string,
-  inline: PropTypes,
+  width: PropTypes.string,
+  minHeight: PropTypes.string,
+  maxHeight: PropTypes.string,
+  height: PropTypes.string,
+};
+
+Box.defaultProps = {
+  inline: false,
 };
 
 export default Box;
