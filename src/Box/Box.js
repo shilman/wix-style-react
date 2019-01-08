fba05bafb360e4bf051975088e0154a5d771e54b
nitayneeman <nitayneeman@users.noreply.github.com>
2019-01-08 10:40:44 +0200

    <Box/> - created styles file

--- src/Box/Box.js
+++ src/Box/Box.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+import styles from './Box.scss';
 
 const Box = props => {
-  const { children } = props;
+  const { children, inline } = props;
+  const rootClassNames = classNames(styles.root, {
+    [styles.inline]: inline,
+  });
 
-  return <div>{children}</div>;
+  return (
+    <div className={rootClassNames}>
+      {children}
+    </div>
+  );
 };
 
 Box.displayName = 'Box';
+
+Box.propTypes = {
+  children: PropTypes.node.isRequired,
+  align: PropTypes.string,
+  verticalAlign: PropTypes.string,
+  padding: PropTypes.oneOfType[PropTypes.string, PropTypes.number],
+  margin: PropTypes.oneOfType[PropTypes.string, PropTypes.number],
+  width: PropTypes.string,
+  minWidth: PropTypes.string,
+  maxWidth: PropTypes.string,
+  inline: PropTypes,
+};
 
 export default Box;
